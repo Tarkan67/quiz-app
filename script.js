b1fb773d5345ca7c1be44e76f71b4fdabe0c172f
Tarkan Aldirmaz <tarkan-aldirmaz@web.de>
2022-02-17 18:39:40 +0100

    added bookmark logic, changed classes in bookmark img, put src in classes for the img

--- script.js
+++ script.js
@@ -3,14 +3,14 @@ const answerButton1 = document.querySelector('[data-js=answerButton1]');
 const answer1 = document.querySelector('.answer1');
 
 const bookmark1 = document.querySelector('[data-js=bookmark1]');
-// Bookmark
+// Bookmark Toggle
 bookmark1.addEventListener('click', () => {
-  const imgWhite = 'images/bookmark-white.png';
-  const imgDark = 'images/bookmark-white.png';
-  if (bookmark1.src == imgWhite) {
-    bookmark1.src = imgDark;
+  if (bookmark1.classList.contains('bookmark-src-white') == true) {
+    bookmark1.classList.add('bookmark-src-black');
+    bookmark1.classList.remove('bookmark-src-white');
   } else {
-    bookmark1.src = 'images/bookmark-white.png';
+    bookmark1.classList.remove('bookmark-src-black');
+    bookmark1.classList.add('bookmark-src-white');
   }
 });
 
